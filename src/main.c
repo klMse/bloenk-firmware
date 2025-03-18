4319e7c2765c972e6b272c76d6fc6523b5aea716
klMse <git@dnvrqq.com>
2025-03-18 01:12:33 +0100

    Added fixed LED target
    Also break backwards compatibility a bit: The original firmware allows a
    LED count of at most 254. However the RAM required by this LED count in
    the original firmware exceeds the available RAM of the ATtiny85-20SU by
    quite a lot. If I am not mistaken at least. We limit ourself to 128 LEDs.

--- src/main.c
+++ src/main.c
@@ -6,8 +6,13 @@
 #include <avr/wdt.h>
 #include <stdint.h>
 
-#define LED_DEFAULT_COUNT 4
-#define LED_MAX_COUNT 8
+#ifndef BLOENK_LED_COUNT
+#    define LED_MAX_COUNT 128
+#    define LED_DEFAULT_COUNT 4
+#else
+#    define LED_MAX_COUNT BLOENK_LED_COUNT
+#    define LED_DEFAULT_COUNT BLOENK_LED_COUNT
+#endif
 
 enum bloenk_version {
     BLOENK_LEGACY,
@@ -35,14 +40,14 @@ struct LEDs {
 uint8_t EE_led_count EEMEM = 1;
 static struct LEDs leds;
 
-static usbMsgLen_t handle_legacy(usbRequest_t *req) {
+static usbMsgLen_t handle_legacy(usbRequest_t* req) {
     uint8_t ret = 0;
     uint8_t val;
 
     switch (req->bRequest) {
     case RQ_SET_CURRENT_LED:
         val = req->wValue.bytes[0];
-        if (val <= LED_MAX_COUNT)
+        if (val < leds.led_count)
             leds.legacy.current_led = val;
         break;
 
@@ -64,9 +69,10 @@ static usbMsgLen_t handle_legacy(usbRequest_t *req) {
     case RQ_WRITE_TO_LEDS:
         break;
 
+#ifndef BLOENK_LED_COUNT
     case RQ_SET_LED_COUNT:
         val = req->wValue.bytes[0];
-        if (val == 0 || val > LED_MAX_COUNT)
+        if (val == 0 || val >= LED_MAX_COUNT)
             break;
 
         if (leds.led_count != val) {
@@ -74,6 +80,7 @@ static usbMsgLen_t handle_legacy(usbRequest_t *req) {
             eeprom_update_byte(&EE_led_count, val);
         }
         break;
+#endif
 
     case RQ_GET_LED_COUNT:
         usbMsgPtr = &leds.led_count;
@@ -106,10 +113,14 @@ int main(void) {
     usbInit();
     sei();
 
+#ifndef BLOENK_LED_COUNT
     eeprom_busy_wait();
     leds.led_count = eeprom_read_byte(&EE_led_count);
     if (leds.led_count == 0xFF)
         leds.led_count = LED_DEFAULT_COUNT;
+#else
+    leds.led_count = LED_DEFAULT_COUNT;
+#endif
 
     while (1) {
         wdt_reset();
